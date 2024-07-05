a40e5a0120b920963d7036902c3ba919afab5089
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-07-06 01:47:02 +0700

    Сделал базовые переменные и начал страницу рассчета доставки

---
+++ src/components/pages/calculateDeliveryPage/CalculateDeliveryPage.tsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.scss'
+
+export const CalculateDeliveryPage=()=>{
+    
+    return(
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <div className={styles.imgContainer}>
+                    <img src="/CalculateDeliveryPageImg/Frame.svg" alt="Frame" />
+                </div>
+                <div className={styles.h1Container}>
+                    <h1>
+                        ЦФТ доставка - быстро, удобно, надежно!
+                    </h1>
+                </div>
+                <div className={styles.formContainer}>  
+                    formformformformformform
+                </div>
+            </div>
+        </div>
+        
+    )
+}
